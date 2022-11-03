8b4368cabacb85253deff0948aad74140336dff0
Lennart <lennarteichhorn@googlemail.com>
2022-11-03 09:23:43 +0000

    Keep only appimage nvim, because that was the fastest

--- install.sh
+++ install.sh
@@ -4,32 +4,30 @@ TMPDIR=$(mktemp -d)
 CURRENT=$PWD
 cd $TMPDIR
 
-export 1>&2
-whoami 1>&2
-pwd 1>&2
+# NIXSTART=$(date +%s.%N)
+# source $HOME/.profile
+# nix-env -iA nixpkgs.neovim
+# NIXEND=$(date +%s.%N)
+# NIXDIFF=$(echo "$NIXEND - $NIXSTART" | bc)
+# echo "Install with nix took $NIXDIFF" 1>&2
+# # Install with nix took 12.794308385
 
 
-NIXSTART=$(date +%s.%N)
-source $HOME/.profile
-nix-env -iA nixpkgs.neovim
-NIXEND=$(date +%s.%N)
-NIXDIFF=$(echo "$NIXEND - $NIXSTART" | bc)
-
-echo "Install with nix took $NIXDIFF" 1>&2
-
-APTSTART=$(date +%s.%N)
-sudo apt install --yes neovim
-APTEND=$(date +%s.%N)
-APTDIFF=$(echo "$APTEND - $APTSTART" | bc)
-echo "Install with apt took $APTDIFF" 1>&2
+# APTSTART=$(date +%s.%N)
+# sudo apt install --yes neovim
+# APTEND=$(date +%s.%N)
+# APTDIFF=$(echo "$APTEND - $APTSTART" | bc)
+# echo "Install with apt took $APTDIFF" 1>&2
+# # Install with apt took 5.360583211
 
 
-APPIMAGESTART=$(date +%s.%N)
+# APPIMAGESTART=$(date +%s.%N)
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract >/dev/null
 mkdir -p /home/gitpod/.local/bin
 ln -s $(pwd)/squashfs-root/AppRun /home/gitpod/.local/bin/nvim
-APPIMAGEEND=$(date +%s.%N)
-APPIMAGEDIFF=$(echo "$APPIMAGEEND - $APPIMAGESTART" | bc)
-echo "Install as appimage took $APPIMAGEDIFF" 1>&2
+# APPIMAGEEND=$(date +%s.%N)
+# APPIMAGEDIFF=$(echo "$APPIMAGEEND - $APPIMAGESTART" | bc)
+# echo "Install as appimage took $APPIMAGEDIFF" 1>&2
+# # Install as appimage took 3.029663746
