71f97e34e079635b8eb8364f715ef8811636b01d
Lennart <lennarteichhorn@googlemail.com>
2022-11-03 09:02:55 +0000

    Add some debug output

--- install.sh
+++ install.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
+export 1>&2
+whoami 1>&2
+pwd 1>&2
+
+
 NIXSTART=$(date +%s.%N)
 nix-env -iA nixpkgs.neovim
 NIXEND=$(date +%s.%N)
 NIXDIFF=$(echo "$NIXEND - $NIXSTART" | bc)
 
-echo "Install with nix took $NIXDIFF"
+echo "Install with nix took $NIXDIFF" 1>&2
 
 APTSTART=$(date +%s.%N)
 sudo apt install --yes neovim
 APTEND=$(date +%s.%N)
 APTDIFF=$(echo "$APTEND - $APTSTART" | bc)
-echo "Install with apt took $APTDIFF"
+echo "Install with apt took $APTDIFF" 1>&2
 
 
 APPIMAGESTART=$(date +%s.%N)
@@ -21,4 +26,4 @@ mkdir -p /home/gitpod/.local/bin
 ln -s $(pwd)/nvim.appimage /home/gitpod/.local/bin/nvim
 APPIMAGEEND=$(date +%s.%N)
 APPIMAGEDIFF=$(echo "$APPIMAGEEND - $APPIMAGESTART" | bc)
-echo "Install as appimage took $APPIMAGEDIFF"
+echo "Install as appimage took $APPIMAGEDIFF" 1>&2
