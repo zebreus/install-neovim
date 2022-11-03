8611a655299d9973b596ec3cc47d92c6ffe9e99c
Lennart <lennarteichhorn@googlemail.com>
2022-11-03 09:16:07 +0000

    Fix nix and appimage build

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+TMPDIR=$(mktemp -d)
+CURRENT=$PWD
+cd $TMPDIR
+
 export 1>&2
 whoami 1>&2
 pwd 1>&2
 
 
 NIXSTART=$(date +%s.%N)
+source $HOME/.profile
 nix-env -iA nixpkgs.neovim
 NIXEND=$(date +%s.%N)
 NIXDIFF=$(echo "$NIXEND - $NIXSTART" | bc)
@@ -22,8 +27,9 @@ echo "Install with apt took $APTDIFF" 1>&2
 APPIMAGESTART=$(date +%s.%N)
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract >/dev/null
 mkdir -p /home/gitpod/.local/bin
-ln -s $(pwd)/nvim.appimage /home/gitpod/.local/bin/nvim
+ln -s $(pwd)/squashfs-root/AppRun /home/gitpod/.local/bin/nvim
 APPIMAGEEND=$(date +%s.%N)
 APPIMAGEDIFF=$(echo "$APPIMAGEEND - $APPIMAGESTART" | bc)
 echo "Install as appimage took $APPIMAGEDIFF" 1>&2
